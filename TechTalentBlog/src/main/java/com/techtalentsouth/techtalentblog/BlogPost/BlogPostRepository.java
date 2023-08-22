80af9eed193496d961f8a2e9ff10a49cf9150a9d
Teddy Conyers Jr <tc3941@rit.edu>
2023-08-21 20:58:10 -0400

    Upload blog

---
+++ TechTalentBlog/src/main/java/com/techtalentsouth/techtalentblog/BlogPost/BlogPostRepository.java
@@ -0,0 +1,5 @@
+package com.techtalentsouth.techtalentblog.BlogPost;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BlogPostRepository extends CrudRepository<BlogPost, Long>{
+}
